49da532bbdffcd4c1762fe50ece689f7388c7bdb
Michael Wagner <MichaelWagner42@gmail.com>
2024-04-13 09:07:40 +0200

    Added folder scanning

--- gphoto_fixer/main.py
+++ gphoto_fixer/main.py
@@ -2,6 +2,8 @@ import argparse
 import logging
 
 takeout_base_folder = None
+files_to_process = []
+image_files = []
 
 
 def analyse_arguments():
@@ -23,8 +25,31 @@ def analyse_arguments():
     print(f"Will work in folder {takeout_base_folder}")
 
 
+def is_json_file(file: str):
+    return file.lower().endswith('.json')
+
+
+def scan_directory(dir_to_scan):
+    import os
+    with os.scandir(dir_to_scan) as scan:
+        for entry in scan:
+            if entry.is_file():
+
+                if is_json_file(entry.name):
+                    files_to_process.append(entry.path)
+                else:
+                    image_files.append(entry.path)
+
+            elif entry.is_dir():
+                scan_directory(entry)
+
+
 def main():
     analyse_arguments()
+
+    print(f'Scanning folder {takeout_base_folder}...')
+    scan_directory(takeout_base_folder)
+    print(f'Found {len(files_to_process)} json files and {len(image_files)} image files')
 
 
 if __name__ == "__main__":
